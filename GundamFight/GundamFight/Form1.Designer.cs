04025da78b4242105019734738b8d508784a5b4d
김지성 <jskim6687@paran.com>
2017-04-12 09:44:49 +0900

    건담 사진 추가

--- GundamFight/GundamFight/Form1.Designer.cs
+++ GundamFight/GundamFight/Form1.Designer.cs
@@ -28,21 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gundamPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gundamPicture)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gundamPicture
+            // 
+            this.gundamPicture.Image = ((System.Drawing.Image)(resources.GetObject("gundamPicture.Image")));
+            this.gundamPicture.Location = new System.Drawing.Point(13, 13);
+            this.gundamPicture.Name = "gundamPicture";
+            this.gundamPicture.Size = new System.Drawing.Size(343, 309);
+            this.gundamPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gundamPicture.TabIndex = 0;
+            this.gundamPicture.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(701, 519);
+            this.Controls.Add(this.gundamPicture);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gundamPicture)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox gundamPicture;
     }
 }
 
